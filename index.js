6a7ee8f8a5da7f5f315b595a93e9298c4209ce1c
ShagunBhatt <36375211+ShagunBhatt@users.noreply.github.com>
2019-09-27 12:16:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!I am checking push event");
+    response.end("Hello World!I am from the master");
 });
 
 const port = process.env.PORT || 1337;
